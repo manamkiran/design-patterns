c3b5e45e7870c88db5f10ab20ba8a5abd7ea2248
manamkiran <manamkiranyadav@gmail.com>
2020-06-22 14:54:22 +0530

    Initial commit

---
+++ src/main/java/designpatterns/DateFormatTest.java
@@ -0,0 +1,12 @@
+package designpatterns;
+
+import java.text.DateFormat;
+
+public class DateFormatTest {
+	
+	public static void main(String[] args) {
+		//DateFormat.getDateInstance().format(null);
+		System.out.println(((Integer) null).toString());
+	}
+
+}
